92dba2aac2016c0b06f01d4a393f587a0a4cdb23
TP <pankowski.tomasz@prokonto.pl>
2022-11-13 10:58:18 +0100

    og, gpdr

--- src/components/services-s1.js
+++ src/components/services-s1.js
@@ -9,8 +9,8 @@ class ServicesS1 extends Component{
             <Container fluid className="services-s1 d-flex minh-50vh align-items-center py-5">
                 <Row className="mx-0 w-100 mt-5">
                     <Col xs={12} md={6} className="minh-25vh d-none d-md-block"></Col>
-                    <Col xs={12} md={6} className="minh-50vh d-flex align-items-center">
-                        <div className="w-100 text-center text-md-start pt-5 text-shadow">
+                    <Col xs={12} md={6} className="minh-25vh d-flex align-items-center">
+                        <div className="w-100 text-center text-md-start text-shadow">
                             <h2 className="dispaly-6 fw-bold text-white-50 text-shadow">
                                 Dolor sit amet         
                             </h2>
